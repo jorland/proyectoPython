4a3d809df71f7fb8b416bcb0eee3e1b3b2af7172
jorland <jorland29rp@hotmail.com>
2015-07-29 15:23:56 -0600

    Initial commit

---
+++ proyectoPython.py
@@ -0,0 +1,12 @@
+__author__ = 'jorland'
+
+
+class producto:
+        codigo=0
+        descripcion=""
+        precio=0
+
+        def __init__(self, codigo, descripcion, precio):
+            self.codigo = codigo
+            self.descripcion = descripcion
+            self.precio = precio
